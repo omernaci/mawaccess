0744d238dfe288d11f522c6edbdfecb60b77a2a4
omernaci <soydemir.naci@gmail.com>
2021-01-17 00:48:34 +0300

    Revert "mawaccess ui material removed, bootsrap added."
    This reverts commit 9bfeeff7f6efced90dfbe6f5cb24b28c37df904b.

--- mawaccess-ui/src/app/app.module.ts
+++ mawaccess-ui/src/app/app.module.ts
@@ -4,21 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ProjectListComponent } from './components/project-list/project-list.component';
-import { ProjectDetailComponent } from './components/project-detail/project-detail.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProjectListComponent,
-    ProjectDetailComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    NgbModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
